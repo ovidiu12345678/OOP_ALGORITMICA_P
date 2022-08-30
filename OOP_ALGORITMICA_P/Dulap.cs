76ef7baef51fb2913d6f663ddf7d42a40be1e5c6
Ovidiu Anton Anton OVIDIUs <ovidiugabriel123456789@outlook.com>
2022-08-30 09:35:51 +0300

    Add project files.

---
+++ OOP_ALGORITMICA_P/Dulap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ALGORITMICA_P
+{
+    public class Dulap
+    {
+        private readonly int lungime;
+        private readonly int latime;
+        private readonly int inaltime;
+
+        public Dulap(int lungime, int latime, int inaltime)
+        {
+            this.lungime = lungime;
+            this.latime = latime;
+            this.inaltime = inaltime;
+        }
+
+        public void Tipareste()
+        {
+            string tiparesteText = "Introduce-ti dimensiunile unui dulap ! ";
+            Console.WriteLine(tiparesteText);
+        }
+
+        public void GetDulapSuprafataTotala()
+        {
+            int  volum = 0;
+            volum += volum * lungime * latime * inaltime;
+        }
+    }
+}
---
+++ OOP_ALGORITMICA_P/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_ALGORITMICA_P
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dulap dulap = CitireDimensiuniDulap();
+          
+        }
+
+
+        public static Dulap CitireDimensiuniDulap()
+        {
+            
+            Console.WriteLine("Setati lungimia dulapu-lui !");
+            int lungime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Setati latimea dulapu-lui ! ");
+            int latime = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Setati inaltimea dulapu-lui ! ");
+            int inaltime = int.Parse(Console.ReadLine());
+            
+            Dulap dulap = new Dulap(lungime, latime, inaltime);
+            dulap.GetDulapSuprafataTotala();
+
+
+            return dulap;
+        }
+
+        
+    }
+}
