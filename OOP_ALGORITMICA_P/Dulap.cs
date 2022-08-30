cdd7b466d4ef9de5e6a44c92d0f190307c8f2540
Ovidiu Anton Anton OVIDIUs <ovidiugabriel123456789@outlook.com>
2022-08-30 23:22:01 +0300

    frr

--- OOP_ALGORITMICA_P/Dulap.cs
+++ OOP_ALGORITMICA_P/Dulap.cs
@@ -1,34 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace OOP_ALGORITMICA_P
+
+namespace OOP_ALGORITMICA_P_Dulap
 {
     public class Dulap
     {
         private readonly int lungime;
         private readonly int latime;
         private readonly int inaltime;
+        public readonly int volum; // volumul poate fi citit de oriunde fiinda e calculat deja 
 
         public Dulap(int lungime, int latime, int inaltime)
         {
             this.lungime = lungime;
             this.latime = latime;
             this.inaltime = inaltime;
-        }
-
-        public void Tipareste()
-        {
-            string tiparesteText = "Introduce-ti dimensiunile unui dulap !";
-            Console.WriteLine(tiparesteText);
-        }
-
-        public void GetDulapSuprafataTotala()
-        {
-            int  volum = 0;
-            volum += volum * lungime * latime * inaltime;
+            this.volum = lungime * latime * inaltime; // volumul e creat automat in constructor
         }
     }
 }
--- OOP_ALGORITMICA_P/Program.cs
+++ OOP_ALGORITMICA_P/Program.cs
@@ -1,40 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace OOP_ALGORITMICA_P
+namespace OOP_ALGORITMICA_P_Dulap
 {
     public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            
-            Dulap dulap = CitireDimensiuniDulap();
-          
+
+            Dulap dulap = CreazaDulap();
+            Console.WriteLine($"Volumul dulapului este {dulap.volum}");
         }
 
 
-        public static Dulap CitireDimensiuniDulap()
+        private static Dulap CreazaDulap()
         {
-            
+            Console.WriteLine("Introduce-ti dimensiunile unui dulap !");
+
             Console.WriteLine("Setati lungimia dulapu-lui !");
             int lungime = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Setati latimea dulapu-lui ! ");
+            Console.WriteLine("Setati latimea dulapu-lui !");
             int latime = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Setati inaltimea dulapu-lui ! ");
+            Console.WriteLine("Setati inaltimea dulapu-lui !");
             int inaltime = int.Parse(Console.ReadLine());
-            
-            Dulap dulap = new Dulap(lungime, latime, inaltime);
-            dulap.GetDulapSuprafataTotala();
 
-
-            return dulap;
+            return new Dulap(lungime, latime, inaltime);
         }
-
-        
     }
 }
